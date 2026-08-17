13ac744ccd83fef3d8377a26a32ea9c31ebaef27
Vijayasankar <vijayasankar2225@gmail.com>
2026-08-17 14:52:43 +0530

    Array Example Class Added

---
+++ src/ArrayExample.java
@@ -0,0 +1,10 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        int[] arr = {3,4,4,6,7};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
