c87ea809ccca894a3ef230f8fd176831fe7f2774
Vijayasankar <vijayasankar2225@gmail.com>
2026-08-18 15:11:01 +0530

    Two Sum Problem

---
+++ src/importantpatterns/TwoSum.java
@@ -0,0 +1,22 @@
+package importantpatterns;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int [] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+    return null;}
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 5;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+}
